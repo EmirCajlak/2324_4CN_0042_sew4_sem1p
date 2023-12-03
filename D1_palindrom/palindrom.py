861de5ffc5491cf17470d631dfcc26017b406e52
Cajlakovic <0042@htl.rennweg.at>
2023-12-03 23:02:41 +0100

    plaindrom_product fertig

--- D1_palindrom/palindrom.py
+++ D1_palindrom/palindrom.py
@@ -20,8 +20,26 @@ def is_palindrom_sentence(s:str):
     :param s: Der String, den man überprüfen soll
     :return: True, wenn der Satz palindrom ist, false wenn nicht
     """
+    # Satz ohne Nicht-Buchstaben und Nicht-Ziffern und mit "seinem" Rückwörts verglichen
     erg = ''.join(s.lower() for c in s if s.isalnum())
     return erg == erg[::-1]
+
+def palindrom_product(x: int):
+    """
+    Findet die größte Palindromzahl (kleiner als x), die das Produkt von zwei 3-stelligen Zahlen ist.
+
+    :param x: Obergrenze
+    :return: Größte Palindromzahl
+
+    """
+
+    groesster_palindrom = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            produkt = i * j
+            if 1000 > produkt < x and is_palindrom(str(produkt)):
+                groesster_palindrom = max(groesster_palindrom, produkt)
+    return groesster_palindrom
 
 
 if __name__ == "__main__":

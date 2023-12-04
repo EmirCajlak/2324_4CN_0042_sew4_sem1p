e3fd284ba0ba43cd004393850dd01828f6fdaed5
Cajlakovic <0042@htl.rennweg.at>
2023-12-04 14:46:15 +0100

    rekursiv main test 1

--- D1_palindrom/rekursiv.py
+++ D1_palindrom/rekursiv.py
@@ -8,3 +8,15 @@ def M(n):
         return M(M(n + 11))
     else:
         return n - 10
+
+if __name__ == "__main__":
+    from time import time
+
+    # m_list mit 200 Elementen
+    m_list = [M(n) for n in range(200)]
+
+    # Dictionary m_dict mit 200 Elementen
+    m_dict = {n: M(n) for n in range(200)}
+
+
+    

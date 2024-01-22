ef3bc3588e0e3d1c4eb8d9f2900ae8d2030bf219
Cajlakovic <0042@htl.rennweg.at>
2024-01-22 17:24:26 +0100

    kasiski nicht vollständig fertig

--- UE05_Kasiki/Kasiski.py
+++ UE05_Kasiki/Kasiski.py
@@ -1,43 +1,46 @@
+"""
+Author: Emir Cajlakovic 4CN
+UE05
+"""
+import math
+from collections import Counter
+from typing import List, Tuple
+
+
 class Kasiski:
-    def __init__(self, crypttext):
-        self._crypttext = crypttext
+    def __init__(self, crypttext: str):
+        self.crypttext = crypttext
 
     @property
-    def crypttext(self):
+    def crypttext(self) -> str:
         return self._crypttext
 
-    def dist(self, text, teilstring):
+    @crypttext.setter
+    def crypttext(self, value: str) -> None:
+        self._crypttext = value.lower()
+
+    def dist(self, text: str, substring: str) -> List[int]:
         """
         Berechnet die Abstände zwischen den Wiederholungen des Teilstrings im verschlüsselten Text.
 
         Usage examples:
-        >>> dist("heissajuchei, ein ei", "ei")
+        >>> kasiski_instance = Kasiski("heissajuchei, ein ei")
+        >>> kasiski_instance.dist("heissajuchei, ein ei", "ei")
         [9, 13, 17]
-        >>> dist("heissajuchei, ein ei", "hai")
+        >>> kasiski_instance.dist("heissajuchei, ein ei", "hai")
         []
         """
         positions = []
-        start = 0
+        index = 0
 
-        while True:
-            position = text.find(teilstring, start)
-            if position == -1:
+        while index < len(text):
+            found_index = text.find(substring, index)
+            if found_index == -1:
                 break
-            positions.append(position)
-            start = position + 1
+            positions.append(found_index)
+            index = found_index + 1
 
         distances = [positions[i + 1] - positions[i] for i in range(len(positions) - 1)]
         return distances
 
-
-# Beispiel-Nutzung
-if __name__ == "__main__":
-    # Beispiel verschlüsselter Text
-    crypttext_example = "xdytcyxtetydtgqehnxdytcyxtetydtgqehnxdytcyxtetydtgqehn"
-
-    # Beispiel Klasse Kasiski
-    kasiski_instance = Kasiski(crypttext_example)
-
-    # Beispiel Nutzung der dist-Methode
-    distances = kasiski_instance.dist(crypttext_example, "xdy")
-    print(distances)
+    

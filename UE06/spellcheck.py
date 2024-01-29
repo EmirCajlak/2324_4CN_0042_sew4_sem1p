bcd7d7068887c3d5f3914424d13ab92dabe2b848
Cajlakovic <0042@htl.rennweg.at>
2024-01-29 14:24:13 +0100

    UE06: read all words fertig

--- UE06/spellcheck.py
+++ UE06/spellcheck.py
@@ -5,4 +5,21 @@ UE06
 
 from typing import List, Tuple, Set
 
+def read_all_words(filename: str) -> Set[str]:
+    word_set = set()
 
+    # Verwende den Context Manager 'with', um die Datei zu öffnen und automatisch zu schließen
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            # Teile die Zeile in Wörter auf und füge sie dem Set hinzu
+            words = line.split()
+            word_set.update(words)
+
+    return word_set
+
+# Beispielaufruf
+filename = 'C:\\Users\\User\\Desktop\\4CN\\Softwareentwicklung\\de-en.txt'
+word_set = read_all_words(filename)
+
+# Ausgabe des Sets
+print(word_set)

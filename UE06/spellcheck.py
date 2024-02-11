05f05ca778cb24005440f499c9e13c7bb0722ac0
Cajlakovic <0042@htl.rennweg.at>
2024-02-11 23:26:03 +0100

    UE06: doctests funktioniert ausser correct

--- UE06/spellcheck.py
+++ UE06/spellcheck.py
@@ -5,97 +5,113 @@ UE06
 import string
 from typing import List, Tuple, Set
 # 1)
-def read_all_words(filename: str) -> Set[str]:
+def read_file(filename: str) -> Set[str]:
+    """
+    Liest eine Datei ein und gibt eine Menge von Wörtern zurück.
+
+    :param filename: Der Pfad zur Datei.
+    :return: Eine Menge, die alle Wörter in der Datei enthält.
+    """
     word_set = set()
 
-    # Verwende den Context Manager 'with', um die Datei zu öffnen und automatisch zu schließen
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
-            # Teile die Zeile in Wörter auf und füge sie dem Set hinzu
             words = line.split()
             word_set.update(words)
 
     return word_set
 
-# Beispielaufruf
-filename = 'C:\\Users\\User\\Desktop\\4CN\\Softwareentwicklung\\de-en.txt'
-word_set = read_all_words(filename)
+def split_word(word: str) -> List[Tuple[str, str]]:
+    """
+    Teilt ein Wort in alle möglichen Kombinationen von Anfang und Ende auf.
 
-# Ausgabe des Sets
-print(word_set)
+    :param word: Das zu teilende Wort.
+    :return: Eine Liste von Tupeln, die Anfangs- und Endkombinationen enthalten.
+    >>> split_word("abc")
+    [('', 'abc'), ('a', 'bc'), ('ab', 'c'), ('abc', '')]
+    >>> split_word("abm")
+    [('', 'abm'), ('a', 'bm'), ('ab', 'm'), ('abm', '')]
+    >>> split_word("hallo")
+    [('', 'hallo'), ('h', 'allo'), ('ha', 'llo'), ('hal', 'lo'), ('hall', 'o'), ('hallo', '')]
+    """
+    return [(word[:i], word[i:]) for i in range(len(word) + 1)]
 
-# 2)
-def split_word(wort: str) -> List[Tuple[str, str]]:
-    result = [(wort[:i], wort[i:]) for i in range(len(wort) + 1)]
-    return result
+def edit1(word: str) -> Set[str]:
+    """
+    Findet alle Wörter mit einer Edit-Distanz von eins.
 
-# Beispielaufruf
-word = "abcd"
-result_list = split_word(word)
-print(result_list)
-
-# 3)
-
-def edit1(wort: str) -> Set[str]:
-    # Funktion zum Einfügen eines Buchstabens
-    alphabet = string.ascii_lowercase  # Das Alphabet in Kleinbuchstaben
+    :param word: Das Eingabewort.
+    :return: Eine Menge von Wörtern mit einer Edit-Distanz von eins.
+    """
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
     def insert(head, new_char, tail):
-        # Fügt einen neuen Buchstaben zwischen head und tail ein
         return head + new_char + tail
 
-    # Funktion zum Löschen eines Buchstabens
-    def delete(word: str) -> str:
+    def delete(word):
         return [word[:i] + word[i+1:] for i in range(len(word))]
 
-    # Funktion zum Vertauschen von zwei aufeinanderfolgenden Buchstaben
-    def transpose(word: str) -> str:
+    def transpose(word):
         return [word[:i] + word[i+1] + word[i] + word[i+2:] for i in range(len(word)-1)]
 
-    # Funktion zum Ersetzen eines Buchstabens
     def replace(head, new_char, tail):
-        # Ersetzen Sie den ersten Buchstaben in head durch new_char
         return head + new_char + tail[1:]
 
-    # Alle möglichen Wörter mit einem Fehler weniger
     candidates = set()
 
-    # Ein Buchstabe fehlt
-    candidates.update([tail for head, tail in split_word(wort) if tail])
-
-    # Zwei Buchstaben vertauscht
-    candidates.update([head + tail[1] + tail[0] + tail[2:] for head, tail in split_word(wort) if len(tail) > 1])
-
-    # Ein Buchstabe ersetzt
+    candidates.update([tail for head, tail in split_word(word) if tail])
+    candidates.update([head + tail[1] + tail[0] + tail[2:] for head, tail in split_word(word) if len(tail) > 1])
     candidates.update([replace(head, tail[0], tail[1:]) for head, tail in split_word(word) if tail])
-
-    # Ein Buchstabe eingefügt
     candidates.update([insert(head, new_char, tail) for head, tail in split_word(word) for new_char in alphabet])
 
     return candidates
 
-# Beispielaufruf
-word = "abc"
-result_set = edit1(word)
-print(result_set)
+def edit1_good(word: str, all_words: List[str]) -> Set[str]:
+    """
+    Filtert korrekte Wörter aus den Ergebnissen von edit1.
 
-# 4)
+    :param word: Das Eingabewort.
+    :param all_words: Eine Liste aller korrekten Wörter.
+    :return: Eine Menge korrekter Wörter mit einer Edit-Distanz von eins.
+    """
+    return edit1(word) & set(all_words)
 
-def edit1_good(wort: str, alle_woerter: List[str]) -> Set[str]:
-    return edit1(wort) & set(alle_woerter)
+def edit2_good(wort:str, alle_worter:list[str]) -> set[str]:
+    """ed2= das set was am Ende rauskommen soll, die erste schleife schreibt in e1 alle mögliochen edit 1 fehlern
+    und die zweite Schleife schreibt dann ins endergebnisset alle edit 1 fehlern von den bereits edit 1 fehlern was
+    zu edit 2 fehlern wird und im wörterbuch stehen ins ed2 hinein.
+    """
+    return {ed2 for ed1 in edit1(wort) for ed2 in edit1_good(ed1, alle_worter)}
 
-def correct(word: str, alle_woerter: List[str]) -> Set[str]:
-    candidates = edit1_good(word, alle_woerter) or {word}
-    return candidates
+def correct(word: str, all_words: List[str]) -> Set[str]:
+    """
+    Findet Korrekturen für ein Wort.
 
-# Beispielaufruf
-filename = 'C:\\Users\\User\\Desktop\\4CN\\Softwareentwicklung\\de-en.txt'
-alle_woerter = read_all_words(filename)
-word = 'apple'
+    :param word: Das Eingabewort.
+    :param all_words: Eine Liste aller korrekten Wörter.
+    :return: Eine Menge von Korrekturen für das Eingabewort.
+    >>> alle_woerter = read_file("C:/Users/User/PycharmProjects/python4CN/UE06/de-en.txt")
+    >>> correct("Aalsuppe", all_words)
+    {'aalsuppe'}
+    >>> correct("Alsuppe", all_words)
+    {'aalsuppe'}
+    >>> sorted(correct("Alsupe",all_words))
+    ['aalsuppe', 'absude', 'alse', 'lupe']
+    """
+    word = word.lower()
+    if word in all_words:
+        return {word}
+    ergSet = edit1_good(word, all_words) or edit2_good(word, all_words)
+    return ergSet
 
-# Verwende das correct-Unterprogramm, um die korrekten Wörter zu erhalten
-result_set = correct(word, alle_woerter)
 
-# Ausgabe der Ergebnisse
-print(f"Korrekturen für '{word}': {result_set}")
+# Doctests
+if __name__ == "__main__":
+    word_set:set[str] = read_file("C:\\Users\\User\\PycharmProjects\\python4CN\\UE06\\de-en.txt")
+    erg_word:list[str] =list(word_set)
+    #print(ergSet)
+    print(edit1("Hallo"))
+    print(edit1_good("Alsuppe", erg_word))
+    print(edit2_good("Alsupp", erg_word))
+
 
